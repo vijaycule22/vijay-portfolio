81924ca6c1756e85ab5c599f38c9383597587f72
vijaycule22 <vjvijaykumar0@gmail.com>
2024-05-13 12:45:22 +0530

    mhg

--- src/App.tsx
+++ src/App.tsx
@@ -273,7 +273,7 @@ function App() {
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d109966.53412125268!2d78.48506214638006!3d17.425885924590325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1715237115910!5m2!1sen!2sin"
                   width="100%"
-                  height="400"
+                  height="300"
                   style={{ border: 0 }}
                   allowFullScreen
                   className="rounded-lg shadow-lg"
